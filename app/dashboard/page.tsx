93052b346f9690ee3894c10b08362f6009f1ddba
themuku <murad.mesteliyev3@gmail.com>
2023-10-28 06:37:47 -0700

    db_

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,6 @@ import { fetchRevenue } from "@/app/lib/data";
 
 export default async function Page() {
   const revenue = await fetchRevenue();
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
